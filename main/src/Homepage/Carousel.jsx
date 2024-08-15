c5b54fc7f4245e399f2d3d8dd77aa9853af999c8
Nguyenthanh0 <166728218+Nguyenthanh0@users.noreply.github.com>
2024-08-15 20:33:41 +0700

    first commit

--- main/src/Homepage/Carousel.jsx
+++ main/src/Homepage/Carousel.jsx
@@ -1,46 +1,40 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './Carousel.css'; 
-import car1 from '../images/imghp.png'
-import car2 from '../images/car2.png'
-import car3 from '../images/car3.png'
-import Navbar from '../Components/NavBar';
+import React, { useState, useEffect, useRef } from "react";
+import "./Carousel.css";
+import car1 from "../images/imghp.png";
+import car4 from "../images/car4.png";
+import car3 from "../images/car3.png";
+import Navbar from "../Components/NavBar";
 
-
-const images = [
-    car1, // Thay thế bằng URL hình ảnh thực tế của bạn
-    car2,
-    car3,
-    // Thêm các hình ảnh ô tô khác nếu cần
-  ];
+const images = [car1, car4, car3];
 const Carousel = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-  
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setCurrentIndex((prevIndex) =>
-          prevIndex === images.length - 1 ? 0 : prevIndex + 1
-        );
-      }, 2000); // Thay đổi mỗi 2 giây
-  
-      return () => clearInterval(intervalId); // Dọn dẹp interval khi component unmount
-    }, []);
-  
-    return (
-      <div className="carousel">
-        <img src={images[currentIndex]} alt="Car" className="carousel-image" />
-        {/* <Navbar></Navbar> */}
-        <div className="carousel-indicator">
-          {images.map((_, index) => (
-            <span
-              key={index}
-              className={`indicator-dot ${
-                index === currentIndex ? 'active' : ''
-              }`}
-            ></span>
-          ))}
-        </div>
-      </div>
-    );
-  };
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  export default Carousel
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 2000); // Thay đổi mỗi 2 giây
+
+    return () => clearInterval(intervalId); // Dọn dẹp interval khi component unmount
+  }, []);
+
+  return (
+    <div className="carousel">
+      <img src={images[currentIndex]} alt="Car" className="carousel-image" />
+      {/* <Navbar></Navbar> */}
+      <div className="carousel-indicator">
+        {images.map((_, index) => (
+          <span
+            key={index}
+            className={`indicator-dot ${
+              index === currentIndex ? "active" : ""
+            }`}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- main/src/Homepage/Search.jsx
+++ main/src/Homepage/Search.jsx
@@ -1,42 +1,39 @@
-import React from 'react'
-import './Search.css'
-import MultiRangeSlider from '../MultiRangeSlider/MultiRangeSlider';
+import React from "react";
+import "./Search.css";
+import MultiRangeSlider from "../MultiRangeSlider/MultiRangeSlider";
 import { CiSearch } from "react-icons/ci";
 import { IoLocationOutline } from "react-icons/io5";
 
 function Search() {
   return (
-    <div className='SearchBar'>
-      <div className='tab'>
+    <div className="SearchBar">
+      <div className="tab">
         <h3>ALL</h3>
         <h3>NEW</h3>
         <h3>USED</h3>
       </div>
-      <div  className='search'>
-        <CiSearch className='sicon' />
-        <input type="text" placeholder='Search'/>
-           <select>
-            <option>Model</option>
-        </select> 
+      <div className="search1">
+        <CiSearch className="sicon" />
+        <input type="text" placeholder="Search" />
         <select>
-            <option>Brand</option>
-        </select> 
+          <option>Model</option>
+        </select>
+        <select>
+          <option>Brand</option>
+        </select>
       </div>
-      <div className='location'>
-          <IoLocationOutline className='licon'/>
-          <input type="text" placeholder='Location' />
-          <div className='pricerange'>
-              
-              <MultiRangeSlider></MultiRangeSlider>
-          </div>
-           <div className='searchbtn'>
+      <div className="location">
+        <IoLocationOutline className="licon" />
+        <input type="text" placeholder="Location" />
+        <div className="pricerange">
+          <MultiRangeSlider></MultiRangeSlider>
+        </div>
+        <div className="searchbtn">
           <button>Search</button>
+        </div>
       </div>
-
-      </div>
-     
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
